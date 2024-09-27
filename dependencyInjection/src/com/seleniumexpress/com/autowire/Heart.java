c25f9b49c454e6dbc765db2a453370c54a2a48a1
HarikaPenukonda <penukondaharika@gmail.com>
2024-09-27 15:08:30 -0500

    IOC and DI using setter injection

---
+++ dependencyInjection/src/com/seleniumexpress/com/autowire/Heart.java
@@ -0,0 +1,10 @@
+package com.seleniumexpress.com.autowire;
+
+public class Heart {
+	
+	public void pump() {
+		System.out.println("Your heart is pumping");
+		System.out.println("You are alive");
+	}
+
+}
---
+++ dependencyInjection/src/com/seleniumexpress/com/autowire/Human.java
@@ -0,0 +1,19 @@
+package com.seleniumexpress.com.autowire;
+
+public class Human {
+	
+	public Heart heart;
+
+	public void setHeart(Heart heart) {
+		this.heart = heart;
+	}
+	
+	public void pumping() {
+		if(heart != null) {
+			heart.pump();
+		}else {
+			System.out.println("You are dead");
+		}
+	}
+
+}
