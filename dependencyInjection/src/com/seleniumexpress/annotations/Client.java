ee63934d15686ae320e4eb495ab2b124271a13a7
HarikaPenukonda <penukondaharika@gmail.com>
2024-10-22 14:27:45 -0500

    Configuration and ComponentScan annotations

--- dependencyInjection/src/com/seleniumexpress/annotations/Client.java
+++ dependencyInjection/src/com/seleniumexpress/annotations/Client.java
@@ -1,12 +1,12 @@
 package com.seleniumexpress.annotations;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Client {
 
 	public static void main(String[] args) {
-		ApplicationContext context = new ClassPathXmlApplicationContext("beans4.xml");
+		ApplicationContext context = new AnnotationConfigApplicationContext(CollegeConfig.class);
 		College college = context.getBean("collegeBean",College.class);
 		System.out.println("The college object is created " + college);
 
