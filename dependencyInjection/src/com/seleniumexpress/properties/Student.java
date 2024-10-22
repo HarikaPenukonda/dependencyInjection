5440256bc68cd95cea0c96e959d60693f0bedc75
HarikaPenukonda <penukondaharika@gmail.com>
2024-10-22 13:30:16 -0500

    injecting values using Value annotaion

--- dependencyInjection/src/com/seleniumexpress/properties/Student.java
+++ dependencyInjection/src/com/seleniumexpress/properties/Student.java
@@ -1,17 +1,22 @@
 package com.seleniumexpress.properties;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class Student {
 	
 	private String studentName;
 	private String interestedCourse;
 	private String hobby;
 	
+	@Value("Admin")
 	public void setStudentName(String studentName) {
 		this.studentName = studentName;
 	}
+	@Value("Spring-framework")
 	public void setInterestedCourse(String interestedCourse) {
 		this.interestedCourse = interestedCourse;
 	}
+	@Value("Coding")
 	public void setHobby(String hobby) {
 		this.hobby = hobby;
 	}
